d5044ced26b8b039e595e5f2158bd606911c8114
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-07-19 01:22:22 +0300

    Enter and button search

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Language from "./components/Language";
 function App() {
   const { t } = useTranslation();
   const [createdAtDate, setCreatedAtDate] = useState("");
+  const [searchText, setSearchText] = useState("");
   const [language, setLanguage] = useState("en-US");
   const [responceDate, setResponceDate] = useState("");
 
@@ -14,33 +15,38 @@ function App() {
     if (responceDate) formatDate(responceDate);
   }, [language]);
 
-  const handleSearch = async (e) => {
+  const handleEnterSearch = (e) => {
     if (e.key === "Enter") {
-      const repoURL = e.target.value;
-      const checkURL = /^(https:\/\/)?github\.com\/[\w-]+\/[\w-]+$/.test(
-        repoURL
-      );
-      try {
-        if (checkURL) {
-          const UrlPart = repoURL.split("/");
-
-          const repoName = UrlPart[UrlPart.length - 1];
-          const username = UrlPart[UrlPart.length - 2];
-
-          const getURL =
-            "https://api.github.com/repos/" + username + "/" + repoName;
-          const response = await axios.get(getURL);
-          setResponceDate(response.data.created_at);
-          formatDate(response.data.created_at);
-        } else {
-          alert(repoURL + t("notValid"));
-        }
-      } catch (error) {
-        console.error(error);
-      }
+      handleSearch();
     }
   };
 
+  const handleSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleSearch = async () => {
+    const repoURL = searchText;
+    const checkURL = /^(https:\/\/)?github\.com\/[\w-]+\/[\w-]+$/.test(repoURL);
+    try {
+      if (checkURL) {
+        const UrlPart = repoURL.split("/");
+
+        const repoName = UrlPart[UrlPart.length - 1];
+        const username = UrlPart[UrlPart.length - 2];
+
+        const getURL =
+          "https://api.github.com/repos/" + username + "/" + repoName;
+        const response = await axios.get(getURL);
+        setResponceDate(response.data.created_at);
+        formatDate(response.data.created_at);
+      } else {
+        alert(repoURL + t("notValid"));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const formatDate = (date) => {
     const options = {
@@ -56,11 +62,30 @@ function App() {
       <div className="main">
         <div className="title">{t("title")}</div>
         <div className="sub-title">{t("subtitle")}</div>
-        <input
-          className="search"
-          placeholder="https://github.com/ihsanberkozcan/github-repo-creation-date"
-          onKeyPress={handleSearch}
-        />
+        <div className="search">
+          <input
+            className="search-input"
+            placeholder="https://github.com/ihsanberkozcan/github-repo-creation-date"
+            onKeyPress={handleEnterSearch}
+            onChange={handleSearchText}
+          />
+          <button className="search-button" onClick={handleSearch}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
       {createdAtDate ? (
         <>
@@ -70,8 +95,7 @@ function App() {
       ) : (
         <></>
       )}
-      <Language setLanguage={setLanguage}/>
-     
+      <Language setLanguage={setLanguage} />
     </>
   );
 }
